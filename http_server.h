ce98dde88fd0edcb0b43abd91fa5ada82d635a63
ItisCaleb <happycaleb1212@gmail.com>
2023-04-30 20:29:01 +0800

    Introduce CMWQ to rewrite worker

--- http_server.h
+++ http_server.h
@@ -1,10 +1,25 @@
 #ifndef KHTTPD_HTTP_SERVER_H
 #define KHTTPD_HTTP_SERVER_H
 
+#include <linux/module.h>
+#include <linux/workqueue.h>
 #include <net/sock.h>
+
+#define MODULE_NAME "khttp"
 
 struct http_server_param {
     struct socket *listen_socket;
+};
+
+struct http_service {
+    bool is_stopped;
+    struct list_head worker;
+};
+
+struct khttp {
+    struct socket *sock;
+    struct list_head list;
+    struct work_struct khttp_work;
 };
 
 extern int http_server_daemon(void *arg);
